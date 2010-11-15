c84278d8e778eee1fddaca82fe82b14a5dfd7985
Natalie <Natalie@128.237.242.40>
2010-11-15 16:02:39 -0500

    Trivial change

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,7 +7,7 @@ class BootStrap {
       case "development":
         def jdoe = new User(login:"jdoe", password:"password", name:"John L Doe")
         jdoe.save() 
-        def jsmith = new User(login:"jsmith", password:"wordpass", name:"Jane Smith")
+        def jsmith = new User(login:"jsmith", password:"wordpass", name:"Jane R Smith")
         jsmith.save()              
       break
       case "production":
